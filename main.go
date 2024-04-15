85a61d103d421904bb3156c480f0cea92deb2c0d
Paul Nunez <paulnunezm@gmail.com>
2024-04-16 00:57:36 +0200

    add initial screen

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ import (
 )
 
 type model struct {
-	expected int
-	actual   int
+	expected         int
+	actual           int
+	canStartPomodoro bool
 
 	expectedInput textinput.Model
 }
@@ -22,9 +23,10 @@ func initialModel() *model {
 	inputField.Focus()
 
 	return &model{
-		expected:      0,
-		actual:        0,
-		expectedInput: inputField,
+		expected:         0,
+		actual:           0,
+		expectedInput:    inputField,
+		canStartPomodoro: false,
 	}
 }
 
@@ -38,8 +40,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
+
 		case "ctrl+c", "q":
-			return nil, tea.Quit
+			return m, tea.Quit
+
 		case "enter":
 			num, err := strconv.Atoi(m.expectedInput.Value())
 			if err != nil {
@@ -58,10 +62,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	s := "\n"
 	if m.expected == 0 {
-		return m.expectedInput.View()
+		s += "======================================================\n"
+		s += "=====|| Enter the number of expected pomodoros ||=====\n"
+		s += "======================================================\n\n"
+
+		s += m.expectedInput.View()
+		s += "\n\n======================================================\n"
+		return s
 	} else {
-		return fmt.Sprintf("Value entered -> %d", m.expected)
+		s += "======================================================\n"
+		s += " - Expected pomodoros: %d\n"
+		s += " - Actual pomodoros: %d\n"
+		s += "\n\n\n\nPress <enter> to start pomodoro <q> to quit"
+		s += "\n======================================================"
+		return fmt.Sprintf(s, m.expected, m.actual)
 	}
 }
 
@@ -70,11 +86,11 @@ func main() {
 	m := initialModel()
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
-		log.Fatalf("err: %w", err)
+		log.Fatalf(`err:%w`, err)
 	}
 	defer f.Close()
-	//p := tea.NewProgram(m, tea.WithAltScreen())
-	p := tea.NewProgram(m)
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	// p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
